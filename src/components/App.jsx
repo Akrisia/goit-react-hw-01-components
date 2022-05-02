2d8438ea7f24ea6a8e545fd220e211c25c3d7505
Iryna S <irynaradkovska@gmail.com>
2022-05-03 00:09:01 +0300

    adds all files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import Profile from './Profile';
+import user from './Profile/user.json';
+import Statistics from './Statistics';
+import data from './Statistics/data.json';
+import FriendList from './FriendList';
+import friends from './FriendList/friends.json';
+import TransactionHistory from './TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
+import s from './App.module.css';
+
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.app}>
+      <div className={s.container}>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+
+        <FriendList friends={friends} />
+
+        <TransactionHistory items={transactions} />
+      </div>
     </div>
   );
-};
+}
+
+export default App;
